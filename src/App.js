0b47291a7a6d20a80d6d72d48e7c88403baeb223
Lyla <48696244+summerysaturn@users.noreply.github.com>
2020-09-12 23:31:34 +0100

    feat(mainpage): add repository portion using GitHub REST to fetch repositories from my account

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 
 import {
-  Button
+  Button, Card, Container, Badge
 } from 'react-bootstrap';
 
 import {
@@ -10,6 +10,48 @@ import {
 } from 'react-icons/fa';
 
 import './App.css';
+
+var seedrandom = require('seedrandom');
+var convert = require('color-convert');
+
+class SocialButtons extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.Links = [
+      {
+        name: "Instagram",
+        icon: <FaInstagram />,
+        url: "https://www.instagram.com/summerysaturn/"
+      },
+      {
+        name: "Twitter",
+        icon: <FaTwitter />,
+        url: "https://twitter.com/summerysaturn"
+      },
+      {
+        name: "GitHub",
+        icon: <FaGithub />,
+        url: "https://github.com/summerysaturn"
+      }
+    ]
+  }
+
+  render() {
+    return (
+      <div className="d-flex">
+        {this.Links.map(e =>
+          <SocialButton url={e.url}>
+            {e.icon}
+            <span className="sr-only"> {e.name} Link</span>
+          </SocialButton >
+        )
+        }
+      </div>
+    )
+  }
+}
 
 class SocialButton extends React.Component {
   render() {
@@ -21,6 +63,100 @@ class SocialButton extends React.Component {
         {this.props.children}
       </a>
     );
+  }
+}
+
+class RepoDisplay extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { restData: [] }
+  }
+
+  componentDidMount() {
+    fetch("https://api.github.com/users/summerysaturn/repos")
+      .then(res => res.json())
+      .then((data) => {
+        this.setState({
+          restData: data
+        })
+      })
+  }
+
+  render() {
+    return (
+      <div className="card-columns">
+        {this.state.restData.map(e => (
+          <RepoCard key={"card-" + e.full_name} repoData={e} />
+        ))}
+      </div>
+    );
+  }
+}
+
+class RepoCard extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.url = "";
+    this.state = { img: "" }
+  }
+
+  componentDidMount() {
+    this.url = "https://raw.githubusercontent.com/" +
+      this.props.repoData.full_name + "/" +
+      this.props.repoData.default_branch + "/" +
+      ".github/preview.png"
+
+    this.img = fetch(this.url)
+      .then(res => res.blob())
+      .then((data) => {
+        if (data.type === "image/png") {
+          this.setState({ img: URL.createObjectURL(data) });
+        }
+      })
+      .catch(error => console.log("Image not found:", error))
+  }
+
+  render() {
+
+    function getColour(string) {
+      let hue = seedrandom(string).quick();
+      return convert.hsl.hex(hue * 360, 100, 50);
+    }
+
+    return (
+      <Card>
+        <a href={this.props.repoData.html_url} className="text-reset">
+
+          {this.state.img &&
+            <img src={this.state.img} className="card-img-top" />
+          }
+
+          {!this.state.img &&
+            <img src={process.env.PUBLIC_URL + "/placeholder.png"} className="card-img-top" />
+          }
+
+          <Card.Body>
+            <Card.Text>
+              <h6>{this.props.repoData.name}</h6>
+              <p>{this.props.repoData.description}</p>
+
+              <Badge
+                style={{
+                  color: "white",
+                  backgroundColor: "#" + getColour(this.props.repoData.language)
+                }}
+              >
+                {this.props.repoData.language}
+              </Badge>
+
+            </Card.Text>
+          </Card.Body>
+
+        </a>
+      </Card >
+    )
   }
 }
 
@@ -36,15 +172,6 @@ function App() {
         <div className="d-flex mb-2">
 
           <Button
-            className="m-2 btn-primary"
-            href="https://github.com/summerysaturn"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            See my other work on GitHub
-          </Button>
-
-          <Button
             className="m-2 btn-info"
             href="https://reactjs.org"
             target="_blank"
@@ -53,23 +180,26 @@ function App() {
             Learn React
           </Button>
 
+          <Button
+            className="m-2 btn-info"
+            href="https://getbootstrap.com/docs/4.5/components/alerts/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn Bootstrap
+          </Button>
+
         </div>
 
-        <div className="d-flex">
-          <SocialButton url="https://www.instagram.com/summerysaturn/">
-            <FaInstagram className="socialButton" />
-          </SocialButton>
-
-          <SocialButton url="https://twitter.com/summerysaturn">
-            <FaTwitter />
-          </SocialButton>
-
-          <SocialButton url="https://github.com/summerysaturn">
-            <FaGithub />
-          </SocialButton>
-        </div>
+        <SocialButtons />
 
       </header>
+
+      <Container className="mb-5">
+        <h1 className="mt-5 mb-5">My Repositories</h1>
+        <RepoDisplay />
+      </Container>
+
     </div>
   );
 }

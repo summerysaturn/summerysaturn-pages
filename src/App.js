8ea4978ef75866278c5386f371e72debb9020bab
Lyla <48696244+summerysaturn@users.noreply.github.com>
2020-09-12 15:26:25 +0100

    feat(deps): properly integrate react-bootstrap using CDN link in index. missing core JS (no modals or popups yet)

--- src/App.js
+++ src/App.js
@@ -2,30 +2,40 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import {
+  Button
+} from 'react-bootstrap';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           This page is under development!
         </p>
-        <a
-          className="App-link"
-          href="https://github.com/summerysaturn"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          See my other work on GitHub
-        </a>
+
+        <div className="d-flex justify-content-between">
+
+          <Button
+            href="https://github.com/summerysaturn"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            See my other work on GitHub
+          </Button>
+
+          <Button
+            className="ml-2 btn-info"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </Button>
+
+        </div>
+
       </header>
     </div>
   );
